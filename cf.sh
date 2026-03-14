2377d4bf52957b75d7c3c0d2e349f1500164be64
AutoNV <bismillah4567y@gmail.com>
2026-03-14 19:40:15 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -2,7 +2,7 @@ set -euo pipefail
 CF_ID="ayfa7756@gmail.com"
 CF_KEY="89df648f7990d6d807d6c9ed7dd265d9d4300"
 DOMAIN="vpnindonesia.biz.id"
-SUB="test$((RANDOM % 10 + 1))"
+SUB="changedomain$((RANDOM % 100 + 1))"
 IP=$(cat /usr/bin/ipsave)
 echo "🔎 Memproses subdomain: ${SUB}.${DOMAIN}"
 echo "🌐 IP publik: ${IP}"
